82f21c5043a03f196eb5e94156fb4e1f8384ac75
paulnwachukwu20 <chimapaul42@gmail.com>
2022-09-08 14:53:25 -0700

    codimg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,24 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                                                                                        
 
+                                                                                                                                                                          
 
+/**                                                                                                                                                                       
 
-/**
+* main - print the string in the put function                                                                                                                             
 
- * main - print the string in the put function
- *
- * Description: using the main function
- * this program prints  "Programming is like building a multilingual puzzle
- * Return: 0
+*                                                                                                                                                                         
 
- */
+* Description: using the main function                                                                                                                                    
 
-int main(void)
+* this program prints "programming is like the building a multilingual puzzle                                                                                             
 
-{
+* Return: 0                                                                                                                                                               
 
-	puts("\"Programming is like building a multilingual puzzle");
+*/                                                                                                                                                                        
 
-	return (0);
+int main(void)                                                                                                                                                            
 
+{  
+       puts("\"programming is like building a multilingual puzzle");
+       return (0);
 }
