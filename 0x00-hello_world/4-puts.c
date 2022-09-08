17dc6532323d2982983c9a2afb6f600a55f7894a
paulnwachukwu20 <chimapaul42@gmail.com>
2022-09-08 15:03:12 -0700

    codimg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,13 @@
-#include <stdio.h>                                                                                                                                                        
-
-                                                                                                                                                                          
-
-/**                                                                                                                                                                       
-
-* main - print the string in the put function                                                                                                                             
-
-*                                                                                                                                                                         
-
-* Description: using the main function                                                                                                                                    
-
-* this program prints "programming is like the building a multilingual puzzle                                                                                             
-
-* Return: 0                                                                                                                                                               
-
-*/                                                                                                                                                                        
-
-int main(void)                                                                                                                                                            
-
+#include <stdio.h>                                                              
+                                                                                
+/**                                                                                                
+* main - print the string in the put function                       
+*                                                           
+* Description: using the main function                                                                                                                          
+* this program prints "programming is like the building a multilingual puzzle   
+* Return: 0                                                                     
+*/                                                                                                                                                              
+int main(void)          
 {  
        puts("\"programming is like building a multilingual puzzle");
        return (0);
