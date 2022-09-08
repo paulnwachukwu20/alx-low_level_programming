94dac5f4c2f7f1c8451de5b764bc4e0d488217f9
paulnwachukwu20 <chimapaul42@gmail.com>
2022-09-08 15:40:47 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,35 +3,23 @@
 /**
 *main - print the string in the put function 
 *
-* Description: using the main function
-*
+* Dscription: using the main function
 * this program prints "programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
     int a;
-
-    	long int b;
-
-		long long int c;
-
-			char d;
-
-				float f;
-
-
+    long int b;
+    long long int c;
+    char d;
+    float f;
 
 					printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-
-						printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-
-							printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-
-								printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-
-									printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-
-										return (0);
+					printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	        			printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+					printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+					printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+					return (0);
 
 }
