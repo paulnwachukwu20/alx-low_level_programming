03789419e2f6ce512895f64a0ca0ba70f1f4e7ec
paulnwachukwu20 <chimapaul42@gmail.com>
2022-09-09 16:33:11 -0700

    soul

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+/**
+  * main - Prints 3 combination of numbers
+  *
+  * Description: using the main function
+  * this program prints all possible different combinations of three digits
+  * Return: Always (Success)
+  */
+
+int main(void)
+
+{
+	int c, i, k;
+
+	for (c = '0'; c <= '9'; c++)
+
+	{
+		for (i = '0'; i <= '9'; i++)
+
+		{
+			for (k = '0'; k <= '9'; k++)
+
+			{
+				if (c < i && i < k)
+
+				{
+					putchar(c);
+
+					putchar(i);
+
+					putchar(k);
+
+					if (c != '7')
+
+					{
+						putchar(',');
+
+						putchar(' ');
+
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
