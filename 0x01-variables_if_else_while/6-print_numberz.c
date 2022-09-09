4a81b8e21488a1acc79fbe67b5f9357982921c24
paulnwachukwu20 <chimapaul42@gmail.com>
2022-09-09 15:35:39 -0700

    god

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+/**
+  * main - Prints the numbers since 0 to 9
+  *
+  * Description:using the main function
+  * this program prints all single digit numbers of base 10 starting from 0
+  * Return: Always (Success)
+  */
+
+int main(void)
+
+{
+
+	int n;
+
+
+
+	for (n = '0'; n <= '9'; n++)
+
+	{
+
+		putchar(n);
+
+	}
+
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
