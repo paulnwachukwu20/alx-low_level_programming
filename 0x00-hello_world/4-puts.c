9cb03aa6970dafc1fd00b5694f27a033ab5bc8d2
paulnwachukwu20 <chimapaul42@gmail.com>
2022-09-08 18:25:51 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
-/**                                                                                                                                                                       
 
+
+/**
+
+ * main - print the string in the put function
 * main - print the string in the put function                                                                                                                             
 
 *                                                                                                                                                                         
 
-* Description: using the main function
-* this program prints "programming is like the building a multilingual puzzle"
-* Return: 0 
- */
+* Description: using the main function                                                                                                                                    
 
-int main(void)
+* this program prints "programming is like the building a multilingual puzzle                                                                                             
 
-{
+* Return: 0                                                                                                                                                               
 
-	puts("\"Programming is like building a multilingual puzzle");
+*/                                                                                                                                                                        
 
-	return (0);
+int main(void)                                                                                                                                                            
 
-}
+{ 
+         printf("Programming is like building a multilingual puzzle
+                return (0);                                                                                                                                                        
+
+}                                                                                                                                                                         
+
