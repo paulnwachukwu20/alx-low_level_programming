7e6009f15d727d3b86fd90c5af886ab844616d51
paulnwachukwu20 <chimapaul42@gmail.com>
2022-09-09 14:56:40 -0700

    print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+/**
+  * main - Prints some letters of alphabet
+  *
+  * Description: using the main function
+  * this program prints the alphabet in lowercase, followed by a new line
+  * Return: Always (Success)
+  */
+
+int main(void)
+
+{
+
+	char c;
+
+
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+
+		if (c != 'e' && c != 'q')
+
+		{
+
+			putchar(c);
+
+		}
+
+	}
+
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
