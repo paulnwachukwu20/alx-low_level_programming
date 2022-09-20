e9ab619d25dd9675e27104c2e3a24532dbb1d5fa
paulnwachukwu20 <chimapaul42@gmail.com>
2022-09-20 12:55:04 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,12 +17,9 @@ if (len % 2 == 0)
 
 len = len / 2;
 
-
-
 else
 
 len = (len + 1) / 2;
-
 
 
 while (str[len] != '\0')
@@ -36,5 +33,31 @@ len++;
 }
 
 _putchar('\n');
+
+}
+
+
+
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+
+int _strlen(char *s)
+
+{
+
+int count, inc;
+
+inc = 0;
+
+for (count = 0; s[count] != '\0'; count++)
+
+inc++;
+
+
+return (inc);
+
 
 }
