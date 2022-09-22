6a65fc64055cf90d64c7376d38b078d9d7099897
paulnwachukwu20 <chimapaul42@gmail.com>
2022-09-22 16:12:53 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,12 @@
 *		returns positive number if s1 > s2
 */
 int _strcmp(char *s1, char *s2)
-
 {
-
 	int i = 0, diff = 0;
-
-
 
 	while (1)
 
 	{
-
 		if (s1[i] == '\0' && s2[i] == '\0')
 
 			break;
@@ -28,41 +23,28 @@ int _strcmp(char *s1, char *s2)
 		else if (s1[i] == '\0')
 
 		{
-
 			diff = s2[i];
 
 			break;
-
 		}
 
 		else if (s2[i] == '\0')
-
 		{
-
 			diff = s1[i];
 
 			break;
-
 		}
-
 		else if (s1[i] != s2[i])
-
 		{
-
 			diff = s1[i] - s2[i];
 
 			break;
-
 		}
-
 		else
 
 			i++;
 
-
-
 	}
 
 	return (diff);
-
 }
