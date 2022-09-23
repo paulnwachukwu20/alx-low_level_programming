144e6c5d89d414e618b5d4da70733ef199185c21
paulnwachukwu20 <chimapaul42@gmail.com>
2022-09-22 20:54:12 -0700

    cap

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,13 +8,10 @@
  *
  * Return: returns poiner to @dest
  */
-
 char *_strcat(char *dest, char *src)
-
 {
 
 	int index = 0, dest_len = 0;
-
 
 
 	while (dest[index++])
@@ -25,7 +22,6 @@ char *_strcat(char *dest, char *src)
 	for (index = 0; src[index]; index++)
 
 		dest[dest_len++] = src[index];
-
 
 	return (dest);
 
