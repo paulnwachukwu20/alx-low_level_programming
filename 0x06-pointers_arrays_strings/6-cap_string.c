d26ff1a3088cf99e3d200dbb91063f36045ba07a
paulnwachukwu20 <chimapaul42@gmail.com>
2022-09-23 17:51:12 -0700

    task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,8 +12,7 @@ char *cap_string(char *str)
 	while (str[++index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
-
-			index++;
+		index++;
 
 		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
@@ -30,8 +29,6 @@ char *cap_string(char *str)
 				str[index - 1] == '}')
 
 			str[index] -= 32;
-
 	}
 	return (str);
-
 }
